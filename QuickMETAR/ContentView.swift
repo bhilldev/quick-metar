9904cbbea363ff273382cf1e2f92c3cbbfc6ea49
Brandon Hill <bhilldev@outlook.com>
2022-03-01 14:57:02 -0600

    Added cloud layer presentation logic ( Clouds: ... )

--- QuickMETAR/ContentView.swift
+++ QuickMETAR/ContentView.swift
@@ -10,21 +10,22 @@ import SwiftUI
 
 struct ContentView: View {
     
+    let fontSize: CGFloat = 22
+    
     @State var data = WeatherData(
         altimeter: Altimeter(value: 0.0),
-//        wind_direction: WindDirection(value: 0),
         station: "KMCI" ,
-//        flight_rules: "",
         dewpoint: DewPoint(value: 0),
         temperature: Temperature(value: 0),
         visibility: Visibility(value: 0),
-        clouds: [Clouds(repr: ""), Clouds(repr: "")]
+        clouds: [Clouds(repr: "")]
     )
-    
-    let fontSize: CGFloat = 22
+    //@State var Icao: String
+    @State var Icao: String
+    @State var cloudString: String
     
     func getData() {
-        let urlString = "https://avwx.rest/api/metar/KMCI?token=zc12yYWWj7T7z2FP6GVRKmbFtyhfrwL3-Mb_TLyfS98"
+        let urlString = "https://avwx.rest/api/metar/\(Icao)?token=zc12yYWWj7T7z2FP6GVRKmbFtyhfrwL3-Mb_TLyfS98"
         let url = URL(string: urlString)
         
         URLSession.shared.dataTask(with: url!) { data, _, error in
@@ -43,39 +44,50 @@ struct ContentView: View {
         }.resume()
         
     }
-   
+    
     var body: some View {
+        
         let altimeterRounded = String(format: "%.2f", data.altimeter.value)
-        //Wind, Clouds, Temperature, Dew Point, Altimiter
+        
+        //Order note: Wind, Clouds, Temperature, Dew Point, Altimiter
         VStack(alignment: .leading) {
-//            HStack {
-//                Image(systemName: "magnifyingglass")
-//                TextField("Search...", text: $icao)
-//            }.textFieldStyle(RoundedBorderTextFieldStyle())
-            Text("Station: \(data.station)")
+            TextField(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/, text: $Icao)
+            Text("Station: \(Icao)")
                 .font(.system(size: fontSize))
-//            Text("Flight Rules: \(data.flight_rules)")
-//                .font(.system(size: fontSize))
             Text("Visibility: \(data.visibility.value) sm")
                 .font(.system(size: fontSize))
-            Text("Clouds: \(data.clouds[0].repr) , \(data.clouds[1].repr)")
-                .font(.system(size: fontSize))
+            HStack {
+                //If no cloud layers are present
+                if data.clouds.count == 0 {
+                    Text("Clouds: None")
+                        .font(.system(size: fontSize))
+                //If one or more cloud layers are present
+                } else {
+                    ForEach(0..<data.clouds.count, id: \.self) { i in
+                        if i == 0 {
+                            Text("Clouds: ")
+                                .font(.system(size: fontSize))
+                            Text(data.clouds[0].repr)
+                        } else {
+                            Text(data.clouds[i].repr)
+                        }
+                    }
+                }
+            }
             Text("Temperature: \(data.temperature.value) &deg;C")
                 .font(.system(size: fontSize))
             Text("Dew Point: \(data.dewpoint.value) &deg;C")
                 .font(.system(size: fontSize))
             Text("Altimeter: \(altimeterRounded)inHg")
                 .font(.system(size: fontSize))
-            
-            
-            Button("Refresh data") {self.getData()}
-        }.padding()
         
-    
+            Button("Refresh data") {self.getData()}
+        
+        }.padding()
     }
-}
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView(Icao: "KLWC", cloudString: "")
+        }
     }
 }
--- QuickMETAR/QuickMETARApp.swift
+++ QuickMETAR/QuickMETARApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct QuickMETARApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(Icao: "KLWC", cloudString: "")
         }
     }
 }
